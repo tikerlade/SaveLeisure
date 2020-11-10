bf572aad6c62525d686facca023745fd7384d931
Ivan Kuznetsov <tikerlade@gmail.com>
2020-11-10 17:41:30 +0300

    Primitive bot
    Set up an echo bot with greeting. It's simple enough.

--- README.md
+++ README.md
@@ -1,13 +1,14 @@
 # SaveLeisure :robot:
 [![Build Status](https://travis-ci.com/tikerlade/SaveLeisure.svg?token=QXtXzRqKNghyH5soGYoY&branch=main)](https://travis-ci.com/tikerlade/SaveLeisure)
 [![codecov](https://codecov.io/gh/tikerlade/SaveLeisure/branch/main/graph/badge.svg?token=I8T7ALFRTX)](https://codecov.io/gh/tikerlade/SaveLeisure)
+[![Code style: black](https://img.shields.io/badge/code%20style-black-000000.svg)](https://github.com/psf/black)
+
 
 Telegram bot that will save interesting information that you send to it and which you want to read / watch later. Such information as a blogpost, film, book.
 
 
 ## Scheme of used Database [[link]](https://dbdiagram.io/d/5fa924cf3a78976d7b7b1930)
 ![Database scheme](pics/database_scheme.png)
-
 
 
 
--- save_leisure/wsgi.py
+++ save_leisure/wsgi.py
@@ -7,10 +7,46 @@ For more information on this file, see
 https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
 """
 
+import logging
 import os
 
 from django.core.wsgi import get_wsgi_application
+from telegram.ext import CommandHandler, Filters, MessageHandler, Updater
 
+# Django part
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "save_leisure.settings")
-
 application = get_wsgi_application()
+logging.basicConfig(
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
+)
+
+
+# Bot part
+def start(update, context):
+    """Greeting message from bot after /start received."""
+    context.bot.send_message(
+        chat_id=update.effective_chat.id,
+        text="I'm a SaveLeisure bot, please talk to me!"
+        + "Currently I'm able only to echo your messages :(",
+    )
+
+
+def echo(update, context):
+    """Send user message with his text. Like echo in the mountains"""
+    context.bot.send_message(chat_id=update.effective_chat.id, text=update.message.text)
+
+
+# Basic connection setup
+updater = Updater(token=os.getenv("TELEGRAM_TOKEN"), use_context=True)
+dispatcher = updater.dispatcher
+
+# Handlers initialization
+start_handler = CommandHandler("start", start)
+echo_handler = MessageHandler(Filters.text & (~Filters.command), echo)
+
+# Setting dispatcher to use handlers
+dispatcher.add_handler(echo_handler)
+dispatcher.add_handler(start_handler)
+
+# Start to listen for messages
+updater.start_polling()
